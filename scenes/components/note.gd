b8d555b7524b7c4ef2c6a07d1d7b1cd97c247a5c
Ren <renrror@gmail.com>
2024-02-28 09:22:06 +0700

    change color to be more readable

--- scenes/components/note.gd
+++ scenes/components/note.gd
@@ -21,12 +21,37 @@ const HEIGHT = 10
 const px_per_ms = (605 - HEIGHT) / 500.0
 var x_size
 
+func configure_style():
+	var style: StyleBoxFlat = StyleBoxFlat.new()
+	style.corner_radius_bottom_left = 10
+	style.corner_radius_bottom_right = 10
+	style.corner_radius_top_left = 10
+	style.corner_radius_top_right = 10
+	
+	if type == NoteType.SLIDER:
+		style.bg_color = Color.GREEN
+		style.bg_color.a = 0.25
+	else:
+		if col < 2:
+			style.bg_color = Color("#5BCEFA")
+		else:
+			style.bg_color = Color("#F5A9B8")
+		
+		if colsize == 4 or (col == 1 and colsize == 2):
+			style.bg_color = Color.WHITE
+		
+		if col == 1 and colsize == 3:
+			style.bg_color = Color("#F5A9B8")
+	add_theme_stylebox_override("panel", style)
+	return style
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var new_height = px_per_ms * max((end_time - time), 10)
 	x_size = colsize * WIDTH
 	set_size(Vector2(x_size, new_height))
 	set_position(Vector2(100 * col, -new_height))
+	configure_style()
 	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -43,15 +68,6 @@ func set_type(t: String):
 		x_size = colsize * WIDTH
 		var new_height = px_per_ms * (end_time - time)
 		set_size(Vector2(x_size, new_height))
-
-		var style: StyleBoxFlat = StyleBoxFlat.new()
-		style.bg_color = Color.GREEN
-		style.corner_radius_bottom_left = 10
-		style.corner_radius_bottom_right = 10
-		style.corner_radius_top_left = 10
-		style.corner_radius_top_right = 10
-		style.bg_color.a = 0.25
-		add_theme_stylebox_override("panel", style)
 
 func is_valid_click(clickCol: int):
 	return col == clickCol or (col < clickCol and col + colsize - 1 >= clickCol)
