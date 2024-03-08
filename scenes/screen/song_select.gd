ec833f8f66800033d7dbee71b4d0637788432362
Rendy Arya Kemal <renrror@gmail.com>
2024-03-08 19:58:25 +0700

    remove songs from web ver

--- scenes/screen/song_select.gd
+++ scenes/screen/song_select.gd
@@ -17,48 +17,18 @@ var songs: Array[Song] = [
 			"difficulties": [
 				{"rating": "1", "chart": "res://songs/issen_kounen/beginner.json", "color": beginner },
 				{"rating": "5", "chart": "res://songs/issen_kounen/advanced.json", "color": advanced },
-				{"rating": "8", "chart": "res://songs/issen_kounen/expert.json", "color": expert },
-				{"rating": "?", "chart": "res://songs/issen_kounen/append.json", "color": misc }
 			],
 			"gimmick": null
 		}
 	),
-	Song.parse_json(
-		{
-			"artist": "ああああ",
-			"title": "コドモチック・フィロソフィ",
-			"jacket": "res://songs/childish/jacket.jpg",
-			"audio": "res://songs/childish/audio.mp3",
-			"difficulties":
-			[
-				{"rating": "3", "chart": "res://songs/childish/beginner.json", "color": beginner },
-				{"rating": "7", "chart": "res://songs/childish/advanced.json", "color": advanced },
-				{"rating": "10", "chart": "res://songs/childish/expert.json", "color": expert }
-			],
-			"gimmick": null
-		}
-	),
-	Song.parse_json(
-		{
-			"artist": "DM DOKURO",
-			"title": "Reality Check Through The Skull",
-			"jacket": "res://songs/reality_check/jacket.jpg",
-			"audio": "res://songs/reality_check/audio.mp3",
-			"difficulties": [
-				{"rating": "4", "chart": "res://songs/reality_check/beginner.json", "color": beginner },
-				{"rating": "9", "chart": "res://songs/reality_check/advanced.json", "color": advanced },
-				{"rating": "12", "chart": "res://songs/reality_check/expert.json", "color": expert }
-			],
-			"gimmick": SansGimmick.new()
-		}
-	)
 	#Song.parse_json(
 		#{
 			#"artist": "xi",
 			#"title": "FREEDOM DiVE",
 			#"jacket": "res://songs/fd/jacket.png",
 			#"audio": "res://songs/fd/audio.mp3",
-			#"difficulties": [{"rating": "15", "chart": "res://songs/fd/master.json", "color": master }]
+			#"difficulties": [{"rating": "15", "chart": "res://songs/fd/master.json", "color": master }],
+			#"gimmick": null
 		#}
 	#)
 ]
@@ -71,6 +41,46 @@ var boxes: Array[SongBox] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if not OS.has_feature("web"):
+		songs[0].difficulties.append_array(
+			[
+				Difficulty.parse_json({"rating": "8", "chart": "res://songs/issen_kounen/expert.json", "color": expert }),
+				Difficulty.parse_json({"rating": "?", "chart": "res://songs/issen_kounen/append.json", "color": misc })
+			]
+		)
+		songs.append(
+			Song.parse_json(
+				{
+					"artist": "ああああ",
+					"title": "コドモチック・フィロソフィ",
+					"jacket": "res://songs/childish/jacket.jpg",
+					"audio": "res://songs/childish/audio.mp3",
+					"difficulties":
+					[
+						{"rating": "3", "chart": "res://songs/childish/beginner.json", "color": beginner },
+						{"rating": "7", "chart": "res://songs/childish/advanced.json", "color": advanced },
+						{"rating": "10", "chart": "res://songs/childish/expert.json", "color": expert }
+					],
+					"gimmick": null
+				}
+			)
+		)
+		songs.append(
+			Song.parse_json(
+				{
+					"artist": "DM DOKURO",
+					"title": "Reality Check Through The Skull",
+					"jacket": "res://songs/reality_check/jacket.jpg",
+					"audio": "res://songs/reality_check/audio.mp3",
+					"difficulties": [
+						{"rating": "4", "chart": "res://songs/reality_check/beginner.json", "color": beginner },
+						{"rating": "9", "chart": "res://songs/reality_check/advanced.json", "color": advanced },
+						{"rating": "12", "chart": "res://songs/reality_check/expert.json", "color": expert }
+					],
+					"gimmick": SansGimmick.new()
+				}
+			)
+		)
 	for song in songs:
 		var box: SongBox = song_box.instantiate()
 		box.song = song
